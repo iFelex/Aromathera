b982944daec18299f5caeebd303a5dd1e4e267ce
david-scv <113469120+david-scv@users.noreply.github.com>
2023-11-24 13:58:32 -0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,8 @@
 import {Sequelize} from 'sequelize'
 
 //const db = new Sequelize('database_nombre', 'usuario', 'contraseña',{
-const db = new Sequelize('aromatheratests', 'root', 'root',{
-    host:'localhost',
+const db = new Sequelize('aromatheratests', 'prodAroma', 'tyC23-1F27Cp',{
+    host:'database-1.cuiluu2ndnpr.us-east-2.rds.amazonaws.com',
     dialect: 'mysql'
 })  
 
